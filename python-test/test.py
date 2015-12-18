759f09c58d0240bf82549fb3e299eeda7b6a1353
Michal <s12336@pjwstk.edu.pl>
2015-12-18 13:19:35 +0100

    Adding all the files.

---
+++ python-test/test.py
@@ -0,0 +1,46 @@
+#we set up the path to the lib here
+#you can skip this if you store the files in your project
+import sys
+sys.path.append('../python')
+
+#these are the only imports you really need
+from HTKFeat import MFCC_HTK
+from HTK import HCopy, HTKFile
+
+#we import some extra libs here for comparison and visualization
+import numpy as np
+import matplotlib
+matplotlib.rcParams['backend'] = "Qt4Agg"
+import matplotlib.pyplot as P
+
+#setting up the main class
+mfcc=MFCC_HTK(filter_file='filter.csv')
+
+#here we load the raw audio file
+sig = mfcc.load_raw_signal('file.raw')
+
+#here we calculate the MFCC+energy, deltas and acceleration coefficients
+feat = mfcc.get_feats(sig)
+delta = mfcc.get_delta(feat,2)
+acc = mfcc.get_delta(delta,2)
+
+#here we merge the MFCCs and deltas together to get 39 features
+feat = np.hstack((feat,delta,acc))
+
+#here we use HTK to calculate the same thing
+#you can comment this line if you don't have HTK installed
+HCopy('hcopy.conf','file.raw','file.htk')
+
+#here we load the features generate by the command above
+htk=HTKFile()
+htk.load('file.htk')
+
+#calculating the difference between features
+diff=feat-htk.data
+
+#computing and dsiplaying the maximum difference between the two methods
+print "Maximum difference: "+str(np.max(np.abs(diff)))
+
+#displaying the difference
+P.pcolormesh(diff.T)
+P.show()
---
+++ python/HTK.py
@@ -0,0 +1,166 @@
+import subprocess
+import numpy
+import struct
+
+def HCopy(conf, wav, htk):
+    """ Runs HTK HCopy program to calculate signal parameters.
+
+        Args:
+            conf (string): Path to configuration file.
+
+            wav (string): Path to audio file.
+
+            htk (string): Path to output HTK file.
+
+        Returns:
+            string: Program output and possible errors. None if program didn't run.
+    """
+    output=None
+    try:
+        output=subprocess.check_output(["HCopy","-C",conf,"-T","1",wav,htk])
+    except subprocess.CalledProcessError, e:
+        print 'EXC '+str(e)
+    return output
+
+class HTKFile:
+    """ Class to load binary HTK file.
+
+        Details on the format can be found online in HTK Book chapter 5.7.1.
+
+        Not everything is implemented 100%, but most features should be supported.
+
+        Not implemented:
+            CRC checking - files can have CRC, but it won't be checked for correctness
+
+            VQ - Vector features are not implemented.
+    """
+    
+    data = None
+    nSamples = 0
+    nFeatures = 0
+    sampPeriod = 0
+    basicKind = None
+    qualifiers = None
+    
+    def load(self, filename):
+        """ Loads HTK file.
+
+            After loading the file you can check the following members:
+
+                data (matrix) - data contained in the file
+
+                nSamples (int) - number of frames in the file
+
+                nFeatures (int) - number if features per frame
+
+                sampPeriod (int) - sample period in 100ns units (e.g. fs=16 kHz -> 625)
+
+                basicKind (string) - basic feature kind saved in the file
+
+                qualifiers (string) - feature options present in the file
+
+        """
+        with open(filename, "rb") as f:
+            
+            header = f.read(12)
+            self.nSamples, self.sampPeriod, sampSize, paramKind = struct.unpack(">iihh",header)
+            basicParameter = paramKind & 0x3F            
+            
+            if basicParameter is 0:
+                self.basicKind = "WAVEFORM"
+            elif basicParameter is 1:
+                self.basicKind = "LPC"
+            elif basicParameter is 2:
+                self.basicKind = "LPREFC"
+            elif basicParameter is 3:
+                self.basicKind = "LPCEPSTRA"
+            elif basicParameter is 4:
+                self.basicKind = "LPDELCEP"
+            elif basicParameter is 5:
+                self.basicKind = "IREFC"
+            elif basicParameter is 6:
+                self.basicKind = "MFCC"
+            elif basicParameter is 7:
+                self.basicKind = "FBANK"
+            elif basicParameter is 8:
+                self.basicKind = "MELSPEC"
+            elif basicParameter is 9:
+                self.basicKind = "USER"
+            elif basicParameter is 10:
+                self.basicKind = "DISCRETE"
+            elif basicParameter is 11:
+                self.basicKind = "PLP"
+            else:
+                self.basicKind = "ERROR"
+           
+            self.qualifiers=[]
+            if ((paramKind & 0100) != 0):
+                self.qualifiers.append("E")
+            if ((paramKind & 0200) != 0):
+                qualifiers.append("N")
+            if ((paramKind & 0400) != 0):
+                self.qualifiers.append("D")
+            if ((paramKind & 01000) != 0):
+                self.qualifiers.append("A")
+            if ((paramKind & 02000) != 0):
+                self.qualifiers.append("C")
+            if ((paramKind & 04000) != 0):
+                self.qualifiers.append("Z")
+            if ((paramKind & 010000) != 0):
+                self.qualifiers.append("K")
+            if ((paramKind & 020000) != 0):
+                self.qualifiers.append("0")
+            if ((paramKind & 040000) != 0):
+                self.qualifiers.append("V")
+            if ((paramKind & 0100000) != 0):
+                self.qualifiers.append("T")
+                
+            if "C" in self.qualifiers or "V" in self.qualifiers or self.basicKind is "IREFC" or self.basicKind is "WAVEFORM":
+                self.nFeatures = sampSize / 2
+            else:
+                self.nFeatures = sampSize / 4
+            
+            if "C" in self.qualifiers:
+                self.nSamples -= 4
+            
+            if "V" in self.qualifiers:
+                raise NotImplementedError("VQ is not implemented")
+            
+            self.data=[]
+            if self.basicKind is "IREFC" or self.basicKind is "WAVEFORM":
+                for x in range(self.nSamples):
+                    s = f.read(sampSize)
+                    frame=[]
+                    for v in range(self.nFeatures):
+                        val=struct.unpack_from(">h",s,v*2)/32767.0
+                        frame.append(val[0])
+                    self.data.append(frame)
+            elif "C" in self.qualifiers:
+                
+                A=[]
+                s = f.read(self.nFeatures * 4)
+                for x in range(self.nFeatures):
+                    A.append(struct.unpack_from(">f",s,x*4)[0])
+                B=[]
+                s = f.read(self.nFeatures * 4)
+                for x in range(self.nFeatures):
+                    B.append(struct.unpack_from(">f",s,x*4)[0])
+                
+                for x in range(self.nSamples):
+                    s = f.read(sampSize)
+                    frame=[]
+                    for v in range(self.nFeatures):
+                        frame.append((struct.unpack_from(">h",s,v*2)[0] + B[v]) / A[v])
+                    self.data.append(frame)
+            else:
+                for x in range(self.nSamples):
+                    s = f.read(sampSize)
+                    frame=[]
+                    for v in range(self.nFeatures):
+                        val=struct.unpack_from(">f",s,v*4)
+                        frame.append(val[0])
+                    self.data.append(frame)
+            
+            
+            if "K" in self.qualifiers:
+                print "CRC checking not implememnted..."
